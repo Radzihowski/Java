83c584c8520af2a743b592445e8c14a7a76b4d83
Radzihowski <A.Radzihovskiy@gmail.com>
2020-01-17 07:34:59 +0000

    Day 0: Hello, World

---
+++ HelloWorld.java
@@ -0,0 +1,18 @@
+import java.io.*;
+import java.util.*;
+import java.text.*;
+import java.math.*;
+import java.util.regex.*;
+
+public class HelloWorld {
+
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in); // open scanner
+        String s = scan.nextLine(); // read the next token and save it to 's'
+        scan.close(); // close scanner
+        System.out.println("Hello, World.");
+        System.out.println(s); // print 's' to System.out, followed by a new line
+    }
+}
+
+
