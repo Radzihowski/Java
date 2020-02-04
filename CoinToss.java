92dc31c2a1c9ef349c5d2308de2f75534717628b
Radzihowski <A.Radzihovskiy@gmail.com>
2020-02-04 06:56:45 +0000

    Day 3: Intro to Conditional Statements 2nd part
    Toss a coin

---
+++ CoinToss.java
@@ -0,0 +1,32 @@
+
+package com.company;
+
+import java.util.Random;
+
+public class CoinToss {
+
+    public String tossACoin() {
+        Random rand = new Random();
+        // taking random integer, make it absolute and devide by 2 result of expressiong below 0 or 1
+        int toss = Math.abs(rand.nextInt()) % 2;
+        if ( toss == 0 )
+            return "HEADS";
+        else {
+            return "TAILS";
+        }
+    }
+
+    public static void main(String[] args) {
+        CoinToss game = new CoinToss();
+        System.out.println(game.tossACoin());
+        System.out.println(game.tossACoin());
+        System.out.println(game.tossACoin());
+        System.out.println(game.tossACoin());
+        System.out.println(game.tossACoin());
+        System.out.println(game.tossACoin());
+        System.out.println(game.tossACoin());
+        System.out.println(game.tossACoin());
+        System.out.println(game.tossACoin());
+        System.out.println(game.tossACoin());
+    }
+}
