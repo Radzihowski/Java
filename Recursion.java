49ff34c68e31e3542ecdcb68401d23d6eec42336
Radzihowski <A.Radzihovskiy@gmail.com>
2020-04-10 09:50:43 +0100

    Day 9: Recursion
    Old good recursion
    reviewed:
    Summation;
    Factorial;
    Exponentiation.

---
+++ Recursion.java
@@ -0,0 +1,39 @@
+package Recursion;
+
+public class Recursion {
+
+    public static int Summation(int n) {
+        // Base Case
+        if (n <= 0) {
+            return 0; //additive identity property
+        }
+        // Recursive Case
+        else {
+            return n + Summation(n - 1);
+        }
+    }
+
+    public static int Factorial(int n) {
+        if (n <= 1) {
+            return 1;
+        } else {
+            return n * Factorial(n - 1);
+        }
+    }
+
+    public static int Exponentiation(int n, int p) {
+        if (p <= 0) {
+            return 1;
+        } else {
+            return n * Exponentiation(n, p - 1);
+        }
+    }
+
+    public static void main(String[] args) {
+        System.out.println(Summation(3));
+        System.out.println(Factorial(3));
+        System.out.println(Exponentiation(5, 3));
+
+    }
+
+}
