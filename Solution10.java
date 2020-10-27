3bad3a2d6e5d5fc614fa49f0a6df869033dfffe0
Radzihowski <A.Radzihovskiy@gmail.com>
2020-10-27 07:01:11 +0000

    Day 10: Solution
    Binary Numbers.
    Task
    Given a base- integer, , convert it to binary (base-). Then find and print the base- integer denoting the maximum number of consecutive 's in 's binary representation.

---
+++ Solution10.java
@@ -0,0 +1,31 @@
+import java.util.*;
+
+public class Solution10 {
+
+    private static final Scanner scanner = new Scanner(System.in);
+
+    public static void main(String[] args) {
+        int n = scanner.nextInt();
+        scanner.skip("(\r\n|[\n\r\u2028\u2029\u0085])?");
+
+        scanner.close();
+
+        int sum = 0;
+        int max = 0;
+
+        while (n > 0) {
+            if (n % 2 == 1) {
+                sum++;
+                if (sum > max) {
+                    max = sum;
+                }
+            } else {
+                    sum = 0;
+                    }
+                n = n / 2;
+            }
+            System.out.println(max);
+        }
+    }
+
+
