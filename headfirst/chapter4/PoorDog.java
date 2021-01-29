b0f14d93f27705a407597e03495f1a1af472d43a
Radzihowski <A.Radzihovskiy@gmail.com>
2021-01-29 11:57:17 +0000

    Added poorly build class
    1) Declared two instance variables, but didn't assign a value;
    2) Result: class compiled but instance variables has default values.

---
+++ headfirst/chapter4/PoorDog.java
@@ -0,0 +1,13 @@
+package com.company.headfirst.chapter4;
+
+public class PoorDog {
+    private int size;
+    private String name;
+
+    public int getSize() {
+        return size;
+    }
+    public String getName() {
+        return name;
+    }
+}
