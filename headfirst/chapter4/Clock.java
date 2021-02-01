14f08e05783ed750246e8723bce05bf536738b20
Radzihowski <A.Radzihovskiy@gmail.com>
2021-02-01 15:24:49 +0000

    Exercises: Be the Compiler

---
+++ headfirst/chapter4/Clock.java
@@ -0,0 +1,13 @@
+package com.company.headfirst.chapter4;
+
+public class Clock {
+    String time;
+
+    void setTime(String t) {
+        time = t;
+    }
+
+    String getTime() {
+        return time;
+    }
+}
