cf559f58b5c50495326609059fdcb1a613f84df1
Radzihowski <a.radzihovskiy@gmail.com>
2021-03-18 17:41:38 +0000

    The final version of the DotCom class

--- headfirst/chapter6/DotCom.java
+++ headfirst/chapter6/DotCom.java
@@ -1,11 +1,16 @@
-import java.util.ArrayList;
+import java.util.*;
 
 public class DotCom {
 
     private ArrayList<String> locationCells;
+    private String name;
 
     public void setLocationCells(ArrayList<String> loc) {
         locationCells = loc;
+    }
+
+    public void setName(String n) {
+        name = n;
     }
 
     public String checkYourself(String userInput) {
@@ -13,8 +18,10 @@ public class DotCom {
         int index = locationCells.indexOf(userInput);
         if (index >= 0) {
             locationCells.remove(index);
+
             if (locationCells.isEmpty()) {
                 result = "kill";
+                System.out.println("Ouch! Your sunk " + name + " : (");
             } else {
                 result = "hit";
             }
