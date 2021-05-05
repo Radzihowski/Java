b1a7f44a79ec03a6f834c04797ddf79d014185fc
Radzihowski <a.radzihovskiy@gmail.com>
2021-05-05 11:56:06 +0100

    Solution for https://codingbat.com/prob/p117334 StringSplosion

---
+++ Spam/CodePractice1/StringSplosion.java
@@ -0,0 +1,18 @@
+package Spam.CodePractice1;
+
+public class StringSplosion {
+    public static void main(String[] args) {
+        System.out.println(stringSplosion("Code"));
+        System.out.println(stringSplosion("abc"));
+        System.out.println(stringSplosion("ab"));
+    }
+
+    static String stringSplosion(String str) {
+        String result = "";
+
+        for ( int i = 0; i < str.length(); i++) {
+            result = result + str.substring(0, i+1);
+        }
+        return result;
+    }
+}
