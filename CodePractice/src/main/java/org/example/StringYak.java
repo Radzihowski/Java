f46bb1f99beaa3c18a954b326b852c98fcadeb6e
Radzihowski <a.radzihovskiy@gmail.com>
2021-05-13 16:45:20 +0100

    1) added POM file; 2) Solution for https://codingbat.com/prob/p126212 stringYak; 3) jUnit for https://codingbat.com/prob/p126212.

--- CodePractice/src/main/java/org/example/StringYak.java
+++ CodePractice/src/main/java/org/example/StringYak.java
@@ -2,10 +2,21 @@ package org.example;
 
 public class StringYak {
     public static void main(String[] args) {
-
+        System.out.println(stringYak("yakpak"));
+        System.out.println(stringYak("pakyak"));
+        System.out.println(stringYak("yak123ya"));
     }
 
-    public static int getInt() {
-        return 1;
+    public static String stringYak(String str) {
+        String result = "";
+
+        for (int i = 0; i < str.length(); i++) {
+            if (i + 2 < str.length() && str.charAt(i) == 'y' && str.charAt(i + 2) == 'k') {
+                i = i + 2;
+            } else {
+                result = result + str.charAt(i);
+            }
+        }
+        return result;
     }
 }
--- CodePractice/src/test/java/org/example/StringYakTest.java
+++ CodePractice/src/test/java/org/example/StringYakTest.java
@@ -7,8 +7,38 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 public class StringYakTest {
 
     @Test
-    public void testGetInt() {
-        assertEquals(2, StringYak.getInt(), "Expected value 1");
+    public void testStringYak() {
+        assertEquals("pak", StringYak.stringYak("yakpak"), "Expected value: pak");
+    }
+
+    @Test
+    public void testStringYak1() {
+        assertEquals("pak", StringYak.stringYak("pakyak"), "Expected value: pak");
+    }
+
+    @Test
+    public void testStringYak2() {
+        assertEquals("123ya", StringYak.stringYak("yak123ya"), "Expected value: 123ya");
+    }
+
+    @Test
+    public void testStringYak3() {
+        assertEquals("", StringYak.stringYak("yak"), "Expected value: ");
+    }
+
+    @Test
+    public void testStringYak4() {
+        assertEquals("xxx", StringYak.stringYak("yakxxxyak"), "Expected value: xxx");
+    }
+
+    @Test
+    public void testStringYak5() {
+        assertEquals("HiHi", StringYak.stringYak("HiyakHi"), "Expected value: HiHi");
+    }
+
+    @Test
+    public void testStringYak6() {
+        assertEquals("xxxyyzzz", StringYak.stringYak("xxxyakyyyakzzz"), "Expected value: xxxyyzzz");
     }
 
 }
