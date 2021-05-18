dcf0b8815543982958aec9713830f9e5cf8ef86d
Radzihowski <a.radzihovskiy@gmail.com>
2021-05-18 13:41:05 +0100

    no message

--- lessons7/src/mapExample/MapExercise2.java
+++ lessons7/src/mapExample/MapExercise2.java
@@ -1,2 +1,32 @@
-package mapExample;public class MapExercise2 {
+package mapExample;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class MapExercise2 {
+    public static void main(String[] args) {
+        Map<String, Person> mapOfPeople = new HashMap<>();
+        Person mike = new Person("Mike", "mike@gmail.com");
+        Person shaun = new Person("Shaun", "shaun@gmail.com");
+        Person sally = new Person("Sally", "sally@gmail.com");
+        Person cesar = new Person("Cesar", "cesar@gmail.com");
+
+        mapOfPeople.put(mike.getEmail(), mike);
+        mapOfPeople.put(shaun.getEmail(), shaun);
+        mapOfPeople.put(sally.getEmail(), sally);
+        mapOfPeople.put(cesar.getEmail(), cesar);
+
+        for (String email : mapOfPeople.keySet()) {
+            System.out.println(email);
+        }
+
+        for ( Person person: mapOfPeople.values()) {
+            System.out.println(person);
+        }
+
+        System.out.println("Get Mike " + mapOfPeople.get("mike@gmail.com"));
+        System.out.println("Get Jeff " + mapOfPeople.get("jeff@email.com"));
+        System.out.println("Contains Mike: " + mapOfPeople.containsKey("mike@gmail.com"));
+        System.out.println("Contains Jeff: " + mapOfPeople.containsKey("jeff@email.com"));
+    }
 }
