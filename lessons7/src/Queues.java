6525c2a1d5ae8e595f3cab305daf297be9d6499e
Radzihowski <a.radzihovskiy@gmail.com>
2021-05-18 19:02:55 +0100

    Queues example from lector

---
+++ lessons7/src/Queues.java
@@ -0,0 +1,18 @@
+import java.util.LinkedList;
+import java.util.Queue;
+
+public class Queues {
+    public static void main(String[] args) {
+
+        Queue<String> myQueue = new LinkedList<>();
+
+        myQueue.add("Hi");
+        myQueue.add("Hi");
+        myQueue.add("Hello");
+        myQueue.add("There");
+
+        while ( !myQueue.isEmpty()) {
+            System.out.println(myQueue.poll());
+        }
+    }
+}
