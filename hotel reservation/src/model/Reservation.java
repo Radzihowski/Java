fdb897fe02f39e26184395c3e98e3e73011ead3f
Radzihowski <a.radzihovskiy@gmail.com>
2021-05-20 23:41:11 +0100

    Hotel Reservation Application

---
+++ hotel reservation/src/model/Reservation.java
@@ -0,0 +1,15 @@
+package model;
+
+import java.util.Date;
+
+public class Reservation {
+    Customer customer;
+    IRoom room;
+    Date checkInDate;
+    Date checkOutDate;
+
+    @Override
+    public String toString() {
+        return customer + " " + room + " " + " " + checkInDate + " " + checkOutDate;
+    }
+}
---
+++ hotel reservation/src/service/CustomerService.java
@@ -0,0 +1,18 @@
+package service;
+
+import model.Customer;
+
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.HashMap;
+import java.util.Map;
+
+public class CustomerService {
+    Map<String, Customer> mapOfCustomers = new HashMap<String, Customer>();
+    Collection<Customer> customers = new ArrayList<Customer>();
+
+
+    public void addCustomer(String email, String firstName, String lastName) {
+
+    }
+}
