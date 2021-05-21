b6848270378bb1cfc3f1c143c2e1c6cec6dc0684
Radzihowski <a.radzihovskiy@gmail.com>
2021-05-21 17:55:15 +0100

    Added solution for https://codingbat.com/prob/p167430 has271

---
+++ CodePractice/src/main/java/org/example/Has271.java
@@ -0,0 +1,17 @@
+package org.example;
+
+public class Has271 {
+    public static void main(String[] args) {
+        System.out.println(has271(new int[]{1, 2, 8, 1}));
+    }
+
+    public static boolean has271(int[] nums) {
+        for ( int i = 0; i < nums.length-2; i++ ) {
+            int val = nums[i];
+            if (nums[i+1] == val+5 && Math.abs(nums[i+2] - (val-1)) <= 2) {
+                return true;
+            }
+        }
+        return false;
+    }
+}
