775084ca3daab2f5e378203a944841506f72cac0
Radzihowski <a.radzihovskiy@gmail.com>
2021-05-22 13:33:06 +0100

    no message

--- CodePractice/src/main/java/org/example/HelloName.java
+++ CodePractice/src/main/java/org/example/HelloName.java
@@ -1,2 +1,11 @@
-package org.example;public class HelloName {
+package org.example;
+
+public class HelloName {
+    public static void main(String[] args) {
+
+    }
+
+    public static String helloName(String name) {
+        return "Hello " + name + "!";
+    }
 }
