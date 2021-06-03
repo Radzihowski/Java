4328b88c6987cb62e1512248d12df7795f4e399f
Radzihowski <a.radzihovskiy@gmail.com>
2021-06-03 13:16:23 +0100

    1) Added solution for https://codingbat.com/prob/p197720 left2 2) Added test cases for https://codingbat.com/prob/p197720 left2

---
+++ CodePractice/src/main/java/org/example/Left2.java
@@ -0,0 +1,11 @@
+package org.example;
+
+public class Left2 {
+    public static void main(String[] args) {
+
+    }
+
+    public static String left2(String str) {
+        return str.substring(2) + str.substring(0,2);
+    }
+}
