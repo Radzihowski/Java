b199205622df1dcb22d29c6bcefd5465022801c4
Radzihowski <a.radzihovskiy@gmail.com>
2021-06-03 12:38:34 +0100

    1) Added solution for https://codingbat.com/prob/p143825 nonStart 2) Added all test cases for https://codingbat.com/prob/p143825 nonStart

---
+++ CodePractice/src/main/java/org/example/NonStart.java
@@ -0,0 +1,11 @@
+package org.example;
+
+public class NonStart {
+    public static void main(String[] args) {
+
+    }
+
+    public static String nonStart(String a, String b) {
+        return a.substring(1) + b.substring(1);
+    }
+}
