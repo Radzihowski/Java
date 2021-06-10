470dde9280a11eb49f88142289c3ba55a78200e9
Radzihowski <a.radzihovskiy@gmail.com>
2021-06-10 12:03:40 +0100

    Added solution for https://codingbat.com/prob/p168564 comboString

--- CodePractice/src/main/java/org/example/ComboString.java
+++ CodePractice/src/main/java/org/example/ComboString.java
@@ -1,2 +1,15 @@
-package org.example;public class ComboString {
+package org.example;
+
+public class ComboString {
+    public static void main(String[] array) {
+
+    }
+
+    public static String comboString(String a, String b) {
+        if ( a.length() < b.length()) {
+            return a + b + a;
+        } else {
+            return b + a + b;
+        }
+    }
 }
