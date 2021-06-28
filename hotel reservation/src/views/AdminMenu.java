b2c750c5e791551a40cfbf4a765ecde0f80ae4a3
Radzihowski <a.radzihovskiy@gmail.com>
2021-06-28 14:37:16 +0100

    Added class AdminMenu: 1) added menu selector; 2) added add room functionality;

--- hotel reservation/src/views/AdminMenu.java
+++ hotel reservation/src/views/AdminMenu.java
@@ -1,5 +1,12 @@
 package views;
 
+import api.AdminResource;
+import model.IRoom;
+import model.Room;
+import model.RoomType;
+
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Scanner;
 
 public class AdminMenu {
@@ -27,6 +34,7 @@ public class AdminMenu {
                     } else if (selection == 4) {
                         System.out.println("Add a Room");
                         keepRunning = false;
+                        addARoom(scanner);
                     } else if (selection == 5) {
                         System.out.println("Back to Main Menu");
                         keepRunning = false;
@@ -40,5 +48,39 @@ public class AdminMenu {
                 }
             }
         }
+    }
+
+    private static void addARoom(Scanner input) {
+        AdminResource adminResource = AdminResource.getInstance();
+        String addRoom;
+        int type;
+
+        do {
+            RoomType roomType = null;
+            input.nextLine();
+            System.out.println("Enter room number: ");
+            String roomNumber = input.nextLine();
+            System.out.println("Enter price per night: ");
+            Double roomPrice = input.nextDouble();
+            do {
+                System.out.println("Enter room type: 1 - Single bed, 2 - Double bed");
+                type = input.nextInt();
+                if (type == 1) {
+                    roomType = RoomType.SINGLE;
+                } else if (type == 2) {
+                    roomType = RoomType.DOUBLE;
+                } else {
+                    System.out.println("Invalid user input");
+                }
+            } while (type != 1 && type != 2);
+            IRoom room = new Room(roomNumber, roomPrice, roomType);
+            List<IRoom> rooms = new ArrayList<>();
+            rooms.add(room);
+            adminResource.addRoom(rooms);
+            do {
+                System.out.println("Would you like to add add another room? Y/N");
+                addRoom = input.next().toLowerCase().trim();
+            } while (!addRoom.equals("y") && !addRoom.equals("n"));
+        } while (addRoom.equals("y"));
     }
 }
